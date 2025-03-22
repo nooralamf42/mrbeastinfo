f9a2441451f2d75e41a2aea17deec8850a0a1633
nooralamf42 <nooralamf42@gmail.com>
2025-03-22 14:54:28 +0530

    fixed lint hehe

--- app/mr-beast-games/bgapplicationseason2/formButton.tsx
+++ app/mr-beast-games/bgapplicationseason2/formButton.tsx
@@ -2,12 +2,7 @@ import { steps } from '@/app/store/atoms'
 import { useSetAtom } from 'jotai'
 import React from 'react'
 
-interface FormButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-  notSubmit?: boolean
-}
-
-export default function FormButton({children, notSubmit}: FormButtonProps) {
+export default function FormButton({children, notSubmit}: any) {
     const setNextStep = useSetAtom(steps)
     const clickHandler = ()=> notSubmit ? setNextStep(pre=>pre+1) : ''
   return (
