ec92c015baa3d1c2c7a460a22b88ef9f18471f0f
nooralamf42 <nooralamf42@gmail.com>
2025-03-23 23:27:21 +0530

    fixed white space above in mr-beast-games

--- app/(mr beast games)/mr-beast-games/components/header.tsx
+++ app/(mr beast games)/mr-beast-games/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header>
         <Link href={'/mr-beast-games'}>
-        <Image className='md:w-[525px] w-[308px] md:text-left max-sm:mx-auto max-sm:px-10 mx-20 mt-10' alt='mr beast games logo' width={50} height={20} src='/images/logoBeastGames.webp'/>
+        <Image className='md:w-[525px] w-[308px] md:text-left max-sm:mx-auto max-sm:px-10 mx-20 pt-10' alt='mr beast games logo' width={50} height={20} src='/images/logoBeastGames.webp'/>
         </Link>
     </header>
   )
