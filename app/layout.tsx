293671ed2d16c740eadd8cea040147179c657542
nooralamf42 <nooralamf42@gmail.com>
2025-04-18 22:39:13 +0530

    updated google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-NELD1PQG37"
+          src="https://www.googletagmanager.com/gtag/js?id=G-CN3T1FCDBN "
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -35,7 +35,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-NELD1PQG37');
+            gtag('config', 'G-CN3T1FCDBN ');
           `}
         </Script>
         {children}
